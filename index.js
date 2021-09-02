483f2139f7601d11814314a29530344de8b4a835
ronah289 <ronald.kibet@student.moringaschool.com>
2021-09-02 12:32:22 -0230

    edit leap yr function

--- index.js
+++ index.js
@@ -1,30 +1,22 @@
-function p(){
-    var f = document.getElementById("year").value;
+function p() {
+  var f = document.getElementById("year").value;
+  var m = document.getElementById("output");
+  if (isNaN(f)) {
+    m.innerHTML = f + "... is not a year";
+  } else {
     //alert(f);
-    var m = document.getElementById("output");
-    m.innerHTML = f;
-} 
-
-
-
-/*
-function input(){
-    var x = document.getElementById("find");
-    var z = document.getElementById("output");
-    var y = document.getElementById("year").value;
-
-    y = parseInt(y);
-    return y;
-    y.toString().length === 4?op(y):z.innerHTML(String(y)+"not a year.");
-    function op(y){
-        if(y % 4 === 0 && y % 400 === 0 ){
-            var m = String(y)+ "is a leap year.";
-            z.innerHTML(m);
-        }
-        else{
-            var m = Stringify(y)+ "is not a leap year.";
-            z.innerHTML(m);
-        }
+    var n = f.toString().length;
+    if (n === 4) {
+      if (f % 400 == 0 || f % 4 == 0) {
+        m.innerHTML = f + " ... is leap year";
+      } else {
+        m.innerHTML = f + " .. not a leap year";
+      }
     }
+    //m.innerHTML = f;
+    else {
+      m.innerHTML = f + ":  not a year.";
+      alert("years have 4 digits.");
+    }
+  }
 }
-*/
